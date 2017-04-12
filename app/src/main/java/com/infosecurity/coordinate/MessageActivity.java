1e694d0e7281ca0929950060d0c175acca3ff8ff
LaneK <kubicki.14@osu.edu>
2017-04-11 21:48:54 -0400

    added GET command to pull chat_id, creator, and messages for that chat id. I pull the messages as JSONArray, haven't been able to test yet though

--- app/src/main/java/com/infosecurity/coordinate/MessageActivity.java
+++ app/src/main/java/com/infosecurity/coordinate/MessageActivity.java
@@ -2,14 +2,22 @@ package com.infosecurity.coordinate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,6 +28,7 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
     ArrayAdapter<String> adapter;
     RequestQueue chatQueue = Volley.newRequestQueue(MessageActivity.this);
     String chatId;
+    RequestQueue queue = Volley.newRequestQueue(MessageActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +56,37 @@ public class MessageActivity extends AppCompatActivity implements View.OnClickLi
         // -------------
         //PULLS JSONOBJECT
         // GET to /chats/chat_id to get messages for the chosen chat
+        final String url = "https://morning-anchorage-16263.herokuapp.com/chats/" + chatId;
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Response", response.toString());
+                        try {
+                            JSONObject jsonObject = response;
+                            //VALUES TO PULL HERE.
+                            //-------------
+                            String chat_id = jsonObject.getString("chat_id");
+                            String creator = jsonObject.getString("creator");
+                            JSONArray messages = jsonObject.getJSONArray("messages");
+                            //USE THESE VALUES TO AGGREGATE CHAT MESSAGES TO MESSAGEACTIVITY
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Log.wtf("error", e.getMessage());
+                            //Test commit changes.
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        );
+        queue.add(getRequest);
 
         //POST to /messages with params mac_address, content, chat_id [to create a message]
 
